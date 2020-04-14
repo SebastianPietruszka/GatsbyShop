0ca5dfa5c51c493d17f87f8533c16c985591aea6
Sebastian Pietruszka <sebastian.pietruszka@britenet.com.pl>
2020-04-14 09:22:36 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Shop`,
+    description: `Learning Gatsby.`,
+    author: `@SebastianPietruszka`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +26,10 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby",
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
